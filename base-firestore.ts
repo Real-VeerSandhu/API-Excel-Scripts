35bcd4ee24d35b2aa9dd072ad920ce9f8cf7c5e6
Veer Sandhu <vsandhu@pridegroupenterprises.com>
2023-07-10 13:48:17 -0400

    add base firestore

---
+++ base-firestore.ts
@@ -0,0 +1,21 @@
+function main(workbook: ExcelScript.Workbook) {
+    // Your code here
+    let sheet = workbook.getActiveWorksheet();
+}
+
+function callFirestoreAPI() {
+    const url = 'https://firestore.googleapis.com/v1/projects/firestore-streamlit/databases/(default)/documents/posts/Apple';
+
+    const httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', url, false);
+
+    httpRequest.send();
+
+    if (httpRequest.status === 200) {
+        console.log('http:', httpRequest.responseText);
+    } else {
+        console.log('Error:', httpRequest.status);
+    }
+}
+
+callFirestoreAPI();
