325c916fbf72e9a28ed3fe8253e0cde9844d786d
Veer Sandhu <sandhu.s.veer@gmail.com>
2023-07-12 10:39:18 -0400

    fix transform object

---
+++ Pipeline/transform-object.ts
@@ -0,0 +1,39 @@
+interface FullMap {
+    key1?: any;
+    key2?: any;
+    key3?: any;
+    // ...
+  }
+  
+function transformObject(input: FullMap): FullMap {
+const transformedObject: FullMap = {} as FullMap;
+
+for (const key in input) {
+    if (Object.prototype.hasOwnProperty.call(input, key)) {
+    transformedObject[key] = input[key] !== undefined ? input[key] : null;
+    }
+}
+
+return transformedObject;
+}
+
+// Example usage
+const input1: FullMap = {
+key1: "value1",
+// key2 is missing
+key3: "value3",
+// ...
+};
+
+const input2: FullMap = {
+// key1 is missing
+key2: "value2",
+key3: "value3",
+// ...
+};
+
+const transformedInput1 = transformObject(input1);
+const transformedInput2 = transformObject(input2);
+
+console.log(transformedInput1);
+console.log(transformedInput2);
