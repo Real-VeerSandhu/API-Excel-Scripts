a3956e84ba936a87261dd7b387ff0e06917a58f3
Veer Sandhu <sandhu.s.veer@gmail.com>
2023-07-12 11:23:52 -0400

    fix map bug

---
+++ Pipeline/full-data2.ts
@@ -0,0 +1,68 @@
+interface FullData1 {
+    id?: string | null;
+    sNo?: number | null;
+    leaseNo?: string | null;
+    sequence?: string | null;
+    applicant?: string | null;
+    operatingName?: string | null;
+    firstName?: string | null;
+    lastName?: string | null;
+    address?: string | null;
+    city?: string | null;
+    province?: string | null;
+    postal?: string | null;
+    homePhone?: string | null;
+    businessPhone?: string | null;
+    cellPhone?: string | null;
+    salesPerson?: string | null;
+    year?: number | null;
+    make?: string | null;
+    model?: string | null;
+    vin?: string | null;
+    vendor?: string | null;
+    vinLastSix?: string | null;
+    term?: number | null;
+    remainingTerm?: number | null;
+    remainingTermCalculated?: number | null;
+    lastPaymentDt?: Date | null;
+    lastPaymentDtCalculated?: Date | null;
+    frequency?: string | null; // Frequency
+    approvedOn?: Date | null;
+    firstPaymentDt?: Date | null;
+    sellingPrice?: number | null;
+    gapPremium?: number | null;
+    walkAwayPremium?: number | null;
+    sellingPriceWithSp?: number | null;
+    down?: number | null;
+    adminFeetPine?: number | null;
+    downRcd?: number | null;
+    finAmtWithSp?: number | null;
+    finAmtWithoutSp?: number | null;
+    rental?: number | null;
+    gap?: number | null;
+    pymtBfrTax?: number | null;
+    endOfTerm?: number | null;
+    applicableTaxes?: number | null;
+    taxAmt?: number | null;
+    totalPayment?: number | null;
+    fico?: number | null;
+    bni?: number | null;
+    creditRating?: string | null; // CreditRating
+    status1?: string | null;
+    status2?: string | null;
+    amtRecd?: number | null;
+    papDt?: string | null;
+    leaseAssociate?: string | null;
+    reserveRelease?: number | null;
+    contractType?: string | null; // ContractType
+    walkAway?: number | null;
+    fundedOn?: Date | null;
+    tranche?: string | null;
+    dmfReserve?: number | null;
+    reserveBal?: number | null;
+    releaseDate?: Date | null;
+    misc?: string | null;
+    mileageAllowance?: number | null;
+    maintainedBy?: string | null; // MaintainedBy
+    partOfMultipleUnits?: string | null;
+  }    
--- Pipeline/map.ts
+++ Pipeline/map.ts
@@ -1,101 +1,92 @@
-interface FullData {
-    id?: string | null;
-    sNo?: number | null;
-    leaseNo?: string | null;
-    sequence?: string | null;
-    applicant?: string | null;
-    operatingName?: string | null;
-    firstName?: string | null;
-    lastName?: string | null;
-    address?: string | null;
-    city?: string | null;
-    province?: string | null;
-    postal?: string | null;
-    homePhone?: string | null;
-    businessPhone?: string | null;
-    cellPhone?: string | null;
-    salesPerson?: string | null;
-    year?: number | null;
-    make?: string | null;
-    model?: string | null;
-    vin?: string | null;
-    vendor?: string | null;
-    vinLastSix?: string | null;
-    term?: number | null;
-    remainingTerm?: number | null;
-    remainingTermCalculated?: number | null;
-    lastPaymentDt?: Date | null;
-    lastPaymentDtCalculated?: Date | null;
-    frequency?: string | null; // Frequency
-    approvedOn?: Date | null;
-    firstPaymentDt?: Date | null;
-    sellingPrice?: number | null;
-    gapPremium?: number | null;
-    walkAwayPremium?: number | null;
-    sellingPriceWithSp?: number | null;
-    down?: number | null;
-    adminFeetPine?: number | null;
-    downRcd?: number | null;
-    finAmtWithSp?: number | null;
-    finAmtWithoutSp?: number | null;
-    rental?: number | null;
-    gap?: number | null;
-    pymtBfrTax?: number | null;
-    endOfTerm?: number | null;
-    applicableTaxes?: number | null;
-    taxAmt?: number | null;
-    totalPayment?: number | null;
-    fico?: number | null;
-    bni?: number | null;
-    creditRating?: string | null; // CreditRating
-    status1?: string | null;
-    status2?: string | null;
-    amtRecd?: number | null;
-    papDt?: string | null;
-    leaseAssociate?: string | null;
-    reserveRelease?: number | null;
-    contractType?: string | null; // ContractType
-    walkAway?: number | null;
-    fundedOn?: Date | null;
-    tranche?: string | null;
-    dmfReserve?: number | null;
-    reserveBal?: number | null;
-    releaseDate?: Date | null;
-    misc?: string | null;
-    mileageAllowance?: number | null;
-    maintainedBy?: string | null; // MaintainedBy
-    partOfMultipleUnits?: string | null;
-  }  
-  
-function createNullObject<T>(keys: (keyof T)[]): T {
-const nullObject: Partial<T> = {};
-keys.forEach((key) => {
-    nullObject[key] = null;
-});
-return nullObject as T;
+interface FullDataPlain {
+    id : string
+    sNo: number;
+    leaseNo: string;
+    sequence: string;
+    applicant: string;
+    operatingName: string;
+    firstName: string;
+    lastName: string;
+    address: string;
+    city: string;
+    province: string;
+    postal: string;
+    homePhone: string;
+    businessPhone: string;
+    cellPhone: string;
+    salesPerson: string;
+    year: number;
+    make: string;
+    model: string;
+    vin: string;
+    vendor: string;
+    vinLastSix: string;
+    term: number;
+    remainingTerm: number;
+    remainingTermCalculated: number;
+    lastPaymentDt: Date;
+    lastPaymentDtCalculated: Date;
+    frequency: string; // Frequency
+    approvedOn: Date;
+    firstPaymentDt: Date;
+    sellingPrice: number;
+    gapPremium: number;
+    walkAwayPremium: number;
+    sellingPriceWithSp: number;
+    down: number;
+    adminFeetPine: number;
+    downRcd: number;
+    finAmtWithSp: number;
+    finAmtWithoutSp: number;
+    rental: number;
+    gap: number;
+    pymtBfrTax: number;
+    endOfTerm: number;
+    applicableTaxes: number;
+    taxAmt: number;
+    totalPayment: number
+    fico: number;
+    bni: number;
+    creditRating: string; // CreditRating
+    status1: string;
+    status2: string;
+    amtRecd: number;
+    papDt: string;
+    leaseAssociate: string;
+    reserveRelease: number;
+    contractType: string; // ContractType
+    walkAway: number;
+    fundedOn: Date;
+    tranche: string;
+    dmfReserve: number;
+    reserveBal: number;
+    releaseDate: Date;
+    misc: string;
+    mileageAllowance: number;
+    maintainedBy: string; // MaintainedBy
+    partOfMultipleUnits: string
 }
 
-const fullDataKeys = Object.keys({} as FullData);
-const nullData: FullData = createNullObject<FullData>(fullDataKeys as (keyof FullData)[]);
-  
-  
+type FullDataPlainKeys = keyof FullDataPlain;
 
-const webInput: FullData = {
-    id: 'jasdhflr',
-    endOfTerm: 23,
-    walkAway: 25,
-    mileageAllowance: 455,
-    creditRating: 'yes'
-    // ...
+function getObjectKeys<T>(obj: T): Array<keyof T> {
+  return Object.keys(obj) as Array<keyof T>;
+}
+
+const newInputPartial: Partial<FullDataPlain> = {
+  id: 'twelve',
+  sNo: 234,
+  leaseNo: 'dsfeef',
+  sequence: 'sdfi4',
+  applicant: 'monkey'
 };
 
-// Default object with all keys set to undefined
-const defaultObject: FullData = Object.keys(webInput).reduce((obj, key) => {
-    obj[key] = undefined;
-    return obj;
-  }, {});
-  
-// Merge the webInput with the defaultObject
-const mergedInput = { ...defaultObject, ...webInput };
+const newInput: FullDataPlain = {
+  ...(newInputPartial as FullDataPlain),
+  ...(getObjectKeys<FullDataPlain>({} as FullDataPlain)
+    .filter(key => !(key in newInputPartial))
+    .reduce((acc, key) => ({ ...acc, [key]: null }), {} as Partial<FullDataPlain>)
+  )
+};
 
-console.log(defaultObject);
+console.log(newInput);
