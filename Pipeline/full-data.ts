f86284da6fe480f4d0104083481f1f1e3a8ee98c
Veer Sandhu <sandhu.s.veer@gmail.com>
2023-07-12 10:53:01 -0400

    add default map

---
+++ Pipeline/full-data.ts
@@ -0,0 +1,68 @@
+interface FullDataPlain {
+    id : string
+    sNo: number;
+    leaseNo: string;
+    sequence: string;
+    applicant: string;
+    operatingName: string;
+    firstName: string;
+    lastName: string;
+    address: string;
+    city: string;
+    province: string;
+    postal: string;
+    homePhone: string;
+    businessPhone: string;
+    cellPhone: string;
+    salesPerson: string;
+    year: number;
+    make: string;
+    model: string;
+    vin: string;
+    vendor: string;
+    vinLastSix: string;
+    term: number;
+    remainingTerm: number;
+    remainingTermCalculated: number;
+    lastPaymentDt: Date;
+    lastPaymentDtCalculated: Date;
+    frequency: string; // Frequency
+    approvedOn: Date;
+    firstPaymentDt: Date;
+    sellingPrice: number;
+    gapPremium: number;
+    walkAwayPremium: number;
+    sellingPriceWithSp: number;
+    down: number;
+    adminFeetPine: number;
+    downRcd: number;
+    finAmtWithSp: number;
+    finAmtWithoutSp: number;
+    rental: number;
+    gap: number;
+    pymtBfrTax: number;
+    endOfTerm: number;
+    applicableTaxes: number;
+    taxAmt: number;
+    totalPayment: number
+    fico: number;
+    bni: number;
+    creditRating: string; // CreditRating
+    status1: string;
+    status2: string;
+    amtRecd: number;
+    papDt: string;
+    leaseAssociate: string;
+    reserveRelease: number;
+    contractType: string; // ContractType
+    walkAway: number;
+    fundedOn: Date;
+    tranche: string;
+    dmfReserve: number;
+    reserveBal: number;
+    releaseDate: Date;
+    misc: string;
+    mileageAllowance: number;
+    maintainedBy: string; // MaintainedBy
+    partOfMultipleUnits: string
+}
---
+++ Pipeline/map.ts
@@ -0,0 +1,88 @@
+interface FullData {
+    id?: string;
+    sNo?: number;
+    leaseNo?: string;
+    sequence?: string;
+    applicant?: string;
+    operatingName?: string;
+    firstName?: string;
+    lastName?: string;
+    address?: string;
+    city?: string;
+    province?: string;
+    postal?: string;
+    homePhone?: string;
+    businessPhone?: string;
+    cellPhone?: string;
+    salesPerson?: string;
+    year?: number;
+    make?: string;
+    model?: string;
+    vin?: string;
+    vendor?: string;
+    vinLastSix?: string;
+    term?: number;
+    remainingTerm?: number;
+    remainingTermCalculated?: number;
+    lastPaymentDt?: Date;
+    lastPaymentDtCalculated?: Date;
+    frequency?: string; // Frequency
+    approvedOn?: Date;
+    firstPaymentDt?: Date;
+    sellingPrice?: number;
+    gapPremium?: number;
+    walkAwayPremium?: number;
+    sellingPriceWithSp?: number;
+    down?: number;
+    adminFeetPine?: number;
+    downRcd?: number;
+    finAmtWithSp?: number;
+    finAmtWithoutSp?: number;
+    rental?: number;
+    gap?: number;
+    pymtBfrTax?: number;
+    endOfTerm?: number;
+    applicableTaxes?: number;
+    taxAmt?: number;
+    totalPayment?: number;
+    fico?: number;
+    bni?: number;
+    creditRating?: string; // CreditRating
+    status1?: string;
+    status2?: string;
+    amtRecd?: number;
+    papDt?: string;
+    leaseAssociate?: string;
+    reserveRelease?: number;
+    contractType?: string; // ContractType
+    walkAway?: number;
+    fundedOn?: Date;
+    tranche?: string;
+    dmfReserve?: number;
+    reserveBal?: number;
+    releaseDate?: Date;
+    misc?: string;
+    mileageAllowance?: number;
+    maintainedBy?: string; // MaintainedBy
+    partOfMultipleUnits?: string;
+}
+
+
+const webInput: FullData = {
+    id: 'jasdhflr',
+    endOfTerm: 23,
+    walkAway: 25,
+    mileageAllowance: 455,
+    creditRating: 'yes'
+    // ...
+};
+
+const defaultObject: FullData = Object.keys(webInput).reduce((obj, key) => {
+    obj[key] = undefined;
+    return obj;
+  }, {});
+  
+// Merge the webInput with the defaultObject
+const mergedInput = { ...defaultObject, ...webInput };
+
+console.log(mergedInput);
