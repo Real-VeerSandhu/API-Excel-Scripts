9e4ae68091f75f07e4205be7d216ae54ae75fb0e
Veer Sandhu <sandhu.s.veer@gmail.com>
2023-07-12 10:59:00 -0400

    add null

--- Pipeline/map.ts
+++ Pipeline/map.ts
@@ -1,71 +1,71 @@
 interface FullData {
-    id?: string;
-    sNo?: number;
-    leaseNo?: string;
-    sequence?: string;
-    applicant?: string;
-    operatingName?: string;
-    firstName?: string;
-    lastName?: string;
-    address?: string;
-    city?: string;
-    province?: string;
-    postal?: string;
-    homePhone?: string;
-    businessPhone?: string;
-    cellPhone?: string;
-    salesPerson?: string;
-    year?: number;
-    make?: string;
-    model?: string;
-    vin?: string;
-    vendor?: string;
-    vinLastSix?: string;
-    term?: number;
-    remainingTerm?: number;
-    remainingTermCalculated?: number;
-    lastPaymentDt?: Date;
-    lastPaymentDtCalculated?: Date;
-    frequency?: string; // Frequency
-    approvedOn?: Date;
-    firstPaymentDt?: Date;
-    sellingPrice?: number;
-    gapPremium?: number;
-    walkAwayPremium?: number;
-    sellingPriceWithSp?: number;
-    down?: number;
-    adminFeetPine?: number;
-    downRcd?: number;
-    finAmtWithSp?: number;
-    finAmtWithoutSp?: number;
-    rental?: number;
-    gap?: number;
-    pymtBfrTax?: number;
-    endOfTerm?: number;
-    applicableTaxes?: number;
-    taxAmt?: number;
-    totalPayment?: number;
-    fico?: number;
-    bni?: number;
-    creditRating?: string; // CreditRating
-    status1?: string;
-    status2?: string;
-    amtRecd?: number;
-    papDt?: string;
-    leaseAssociate?: string;
-    reserveRelease?: number;
-    contractType?: string; // ContractType
-    walkAway?: number;
-    fundedOn?: Date;
-    tranche?: string;
-    dmfReserve?: number;
-    reserveBal?: number;
-    releaseDate?: Date;
-    misc?: string;
-    mileageAllowance?: number;
-    maintainedBy?: string; // MaintainedBy
-    partOfMultipleUnits?: string;
-}
+    id?: string | null;
+    sNo?: number | null;
+    leaseNo?: string | null;
+    sequence?: string | null;
+    applicant?: string | null;
+    operatingName?: string | null;
+    firstName?: string | null;
+    lastName?: string | null;
+    address?: string | null;
+    city?: string | null;
+    province?: string | null;
+    postal?: string | null;
+    homePhone?: string | null;
+    businessPhone?: string | null;
+    cellPhone?: string | null;
+    salesPerson?: string | null;
+    year?: number | null;
+    make?: string | null;
+    model?: string | null;
+    vin?: string | null;
+    vendor?: string | null;
+    vinLastSix?: string | null;
+    term?: number | null;
+    remainingTerm?: number | null;
+    remainingTermCalculated?: number | null;
+    lastPaymentDt?: Date | null;
+    lastPaymentDtCalculated?: Date | null;
+    frequency?: string | null; // Frequency
+    approvedOn?: Date | null;
+    firstPaymentDt?: Date | null;
+    sellingPrice?: number | null;
+    gapPremium?: number | null;
+    walkAwayPremium?: number | null;
+    sellingPriceWithSp?: number | null;
+    down?: number | null;
+    adminFeetPine?: number | null;
+    downRcd?: number | null;
+    finAmtWithSp?: number | null;
+    finAmtWithoutSp?: number | null;
+    rental?: number | null;
+    gap?: number | null;
+    pymtBfrTax?: number | null;
+    endOfTerm?: number | null;
+    applicableTaxes?: number | null;
+    taxAmt?: number | null;
+    totalPayment?: number | null;
+    fico?: number | null;
+    bni?: number | null;
+    creditRating?: string | null; // CreditRating
+    status1?: string | null;
+    status2?: string | null;
+    amtRecd?: number | null;
+    papDt?: string | null;
+    leaseAssociate?: string | null;
+    reserveRelease?: number | null;
+    contractType?: string | null; // ContractType
+    walkAway?: number | null;
+    fundedOn?: Date | null;
+    tranche?: string | null;
+    dmfReserve?: number | null;
+    reserveBal?: number | null;
+    releaseDate?: Date | null;
+    misc?: string | null;
+    mileageAllowance?: number | null;
+    maintainedBy?: string | null; // MaintainedBy
+    partOfMultipleUnits?: string | null;
+  }  
 
 
 const webInput: FullData = {
@@ -86,4 +86,4 @@ const defaultObject: FullData = Object.keys(webInput).reduce((obj, key) => {
 // Merge the webInput with the defaultObject
 const mergedInput = { ...defaultObject, ...webInput };
 
-console.log(mergedInput);
+console.log(defaultObject);
